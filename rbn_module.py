c3099d6483ac3793878dfacc6e10c0a6889197ef
Gianni Perez <gianni@Giannis-MacBook-Air.attlocal.net>
2025-08-03 09:29:00 -0400

    reqs uploaded

--- rbn_module.py
+++ rbn_module.py
@@ -2,7 +2,6 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
-
 class RBN:
     def __init__(self, nodes, min_k=1, max_k=20, topology=None):
         """
